8144f25a04292ea4ad9f12fdd8b51282a6ab0475
dmkael <tanmaar@gmail.com>
2024-05-26 23:29:49 +0300

    MOD env settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG").lower() == 'true'
+DEBUG = os.getenv("DEBUG") in ['True', 'TRUE', 'true']
 
 ALLOWED_HOSTS = [
     'python-project-52-4ipl.onrender.com',

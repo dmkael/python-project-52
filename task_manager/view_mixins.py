20bac9f22eef3353db19edff5470e38aa3d5e897
dmkael <tanmaar@gmail.com>
2024-06-14 05:56:22 +0300

    RF view mixins to use builtin ContextMixin

--- task_manager/view_mixins.py
+++ task_manager/view_mixins.py
@@ -1,51 +1,33 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import ProtectedError
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.base import ContextMixin
 from django.views.generic import ListView
 from django.contrib import messages
 from django.shortcuts import redirect
 
 
-class IndexViewMixin(ListView):
-    extra_context = {}
-
-    def get_context_data(self, **kwargs):
-        kwargs.update(extra_context=self.extra_context)
-        return super().get_context_data(**kwargs)
+class IndexViewMixin(ListView, ContextMixin):
+    pass
 
 
-class CreateViewMixin(SuccessMessageMixin, CreateView):
-    extra_context = {}
-
-    def get_context_data(self, **kwargs):
-        kwargs.update(extra_context=self.extra_context)
-        return super().get_context_data(**kwargs)
+class CreateViewMixin(SuccessMessageMixin, CreateView, ContextMixin):
 
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form), status=400)
 
 
-class UpdateViewMixin(SuccessMessageMixin, UpdateView):
-    extra_context = {}
-
-    def get_context_data(self, **kwargs):
-        kwargs.update(extra_context=self.extra_context)
-        return super().get_context_data(**kwargs)
+class UpdateViewMixin(SuccessMessageMixin, UpdateView, ContextMixin):
 
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form), status=400)
 
 
-class DeleteViewMixin(SuccessMessageMixin, DeleteView):
-    extra_context = {}
+class DeleteViewMixin(SuccessMessageMixin, DeleteView, ContextMixin):
     failure_message = 'Cannot delete object because it is in use.'
     redirect_url = '/'
     success_url = '/'
     have_dependencies = False
-
-    def get_context_data(self, **kwargs):
-        kwargs.update(extra_context=self.extra_context)
-        return super().get_context_data(**kwargs)
 
     def post(self, request, *args, **kwargs):
         if not self.have_dependencies:

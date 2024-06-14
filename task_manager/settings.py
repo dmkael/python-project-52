c9ae84037bf083ff2a7cd75da46118caba064205
dmkael <tanmaar@gmail.com>
2024-06-14 23:12:41 +0300

    MOD Rollbar init access_token settings in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -184,7 +184,7 @@ LOCALE_PATHS = (
 
 
 ROLLBAR = {
-    'access_token': os.getenv("ROLLBAR_ACCESS_TOKEN") or 'fake-key',
+    'access_token': os.getenv("ROLLBAR_ACCESS_TOKEN"),
     'environment': 'development' if DEBUG else 'production',
     'code_version': '1.0',
     'root': BASE_DIR,

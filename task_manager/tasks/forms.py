611e2a14e176e04a6a0d53e77b1017d31bed71cf
dmkael <tanmaar@gmail.com>
2024-06-14 18:16:14 +0300

    RF tasks form init

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from task_manager.tasks.models import Task
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+from task_manager.users.models import User
 from django.utils.translation import gettext_lazy as _
 
 
@@ -18,3 +21,12 @@ class TaskForm(forms.ModelForm):
             'executor': forms.Select(),
             'labels': forms.SelectMultiple(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        status_choices = Status.objects.all()
+        executor_choices = User.objects.all()
+        labels_choices = Label.objects.all()
+        self.fields['status'].queryset = status_choices
+        self.fields['executor'].queryset = executor_choices
+        self.fields['labels'].queryset = labels_choices

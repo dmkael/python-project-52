aec4389740f138e9742472fa8773cc792fb20946
dmkael <tanmaar@gmail.com>
2024-06-26 03:03:19 +0300

    FIX build.sh for Render.com

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+# Exit on error
+set -o errexit
+set -o pipefail
+
 # setup docker postgres database
 docker-compose -f docker/compose.yml up -d
 
-# Exit on error
-set -o errexit
+# Check if docker-compose up was successful
+docker_compose_exit_code=$?
+if [ $docker_compose_exit_code -ne 0 ]; then
+    echo "Failed to start Docker containers. Exiting."
+    exit $docker_compose_exit_code
+fi
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 make install
